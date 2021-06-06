244b0f8e7379371ad8278a5fe6845b9f0f43c90b
ashulyak <alexey.shulyak@vrpconsulting.com>
2021-06-06 12:39:59 +0300

    refactor: fixed logger

--- src/middleware/loggerMiddleware.ts
+++ src/middleware/loggerMiddleware.ts
@@ -1,19 +1,29 @@
 import express, { NextFunction } from 'express';
-import { finished } from "stream";
-import fs from "fs";
+import { finished } from 'stream';
+import fs from 'fs';
 
-export default (request: express.Request, response: express.Response, next: NextFunction) => {
+export default (
+  request: express.Request,
+  response: express.Response,
+  next: NextFunction
+) => {
   const { method, url } = request;
   const start = Date.now();
 
   next();
 
   finished(response, () => {
-    const data = `${method} ${url} [${ Date.now() - start }ms] \n`;
+    const data = `
+      Method: ${method}
+      Url: ${url}
+      StatusCode: ${response.statusCode}
+      Body: ${JSON.stringify(request.body)}
+      Params: ${JSON.stringify(request.params)}
+      Time: [${Date.now() - start}ms] \n`;
     fs.appendFile('log.txt', data, (e) => {
       if (e) {
         console.error(e);
       }
-    })
+    });
   });
-}
+};
