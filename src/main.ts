814edd8823453758ff0d161a9030bf2b57c05e5d
ashulyak <alexey.shulyak@vrpconsulting.com>
2021-07-04 19:12:12 +0300

    revove express app

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import { load } from 'yamljs';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const swaggerDocument = load(join(__dirname, '../doc/api.yaml'));
-
   SwaggerModule.setup('doc', app, swaggerDocument);
 
   await app.listen(3000);
