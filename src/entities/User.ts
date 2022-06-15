53d2a1dd631d05cf864f8321166a7612df7a4af4
Aleksey Shulyak <a.shulyak@itransition.com>
2022-06-15 18:20:32 +0400

    2022: Refactor

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -16,3 +16,28 @@ export class User {
   password!: string;
 
 }
+
+// @Entity()
+// export class User {
+//   @PrimaryGeneratedColumn('uuid')
+//   id: Id;
+//
+//   @Column('varchar', { length: 100 })
+//   name: string;
+//
+//   @Column('varchar', { length: 100 })
+//   login: string;
+//
+//   @Column('varchar', { length: 100 })
+//   password: string;
+//
+//   @OneToMany<Task>((_type) => Task, (task: Task): Id => task.userId as Id, { cascade: true })
+//   tasks: Task[];
+//
+//   static toResponse(user?: Partial<User>): Partial<User> | undefined {
+//     if (!user) return undefined;
+//
+//     const { id, name, login } = user;
+//     return { id, name, login };
+//   }
+// }
